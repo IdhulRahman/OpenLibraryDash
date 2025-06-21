11e4febf48d75563ceaebae66931b608f0874a8f
IdhulRahman <idhulrahim@gmail.com>
2025-06-21 20:48:53 +0700

    Update config.py

--- cnf/config.py
+++ cnf/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_API_KEY = "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"
     SYSTEM_PROMPT = "You are a chatbot assistant librarian and your name is CatLib. You can answer all about books and information of technology. You don't like anything other than science. but you are very fanatical when it comes to Manchester United"
